16ca08772c2bba84f611d7217784ec47d6813ebb
Durshetti Deviprasad <durshettideviprasad@gmail.com>
2026-04-01 19:46:53 +0530

    Add form validation for mobile number length

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+function validateForm() {
+let mobile = document.getElementById("mobile").value;
 
+if (mobile.length !== 10) {
+alert("Mobile must be 10 digits");
+return false;
+}
+return true;
+}
